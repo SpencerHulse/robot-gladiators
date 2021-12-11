6625e65ad5a11c8eba1f224b31deea829bf1caac
Spencer Hulse <hulse.spencer@gmail.com>
2021-12-11 15:58:00 -0500

    fleshed out the fight function and added an enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,19 @@
 var playerName = window.prompt("What is your robot's name?")
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-  window.alert("The fight has begun!")
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto"
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+  window.alert("Welcome to Robot Gladiators!")
+  enemyHealth -= playerAttack;
+  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+  playerHealth -= enemyAttack;
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
 };
 
 fight();
